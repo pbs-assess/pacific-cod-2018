5bb691f5865d4ad248d5f4e4dd28d8bf2b0447b0
Chris Grandin <cgrandin@shaw.ca>
2018-08-10 10:46:28 -0700

    Add capitalized extension R files back in. Code from previous commit shoiuld work with these.

---
+++ R/all.R
@@ -0,0 +1,49 @@
+## all.r
+## This is the master file - it loads all packages and sources all
+##  other R source code files.
+## To debug in an R session, run these commands first:
+## source(file.path(here::here(), "R/all.r"));load.models.into.parent.env();source(file.path(here::here(), "R/custom-knitr-variables.r"))
+
+rm(list = ls(all = TRUE))
+
+## Need to do this first to provide install.packages.if.needed()
+## if(!("iscamtext" %in% rownames(installed.packages()))){
+##   devtools::install_github("cgrandin/iscamtex")
+## }
+## library(iscamtex)
+
+## install.packages.if.needed("coda")
+## install.packages.if.needed("knitr")
+## install.packages.if.needed("lubridate")
+## install.packages.if.needed("PBSmodelling")
+## install.packages.if.needed("xtable")
+## install.packages.if.needed("RColorBrewer")
+
+library(here)
+library(dplyr)
+library(ggplot2)
+library(gfplot)
+library(coda)
+library(knitr)
+library(lubridate)
+library(PBSmodelling)
+library(xtable)
+library(tidyverse)
+library(RColorBrewer)
+
+rootd <- here::here()
+source(file.path(rootd, "R/utilities.R"))
+source(file.path(rootd, "R/load-models.R"))
+source(file.path(rootd, "R/verify.R"))
+
+## Code to setup the model names, and start/end years for various things
+##  in the models
+source(file.path(rootd, "R/model-setup.R"))
+
+## Code to setup forecast model runs
+## source("forecast-catch-levels.r")
+## Code to setup retro model runs.
+## source("retrospective-setup.r")
+
+## Set up variables for data tables from csv files
+##source("data-tables.r")
