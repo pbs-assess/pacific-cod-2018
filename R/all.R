807f91a4e9ddc291c526c0ad653b50340db73808
Chris Grandin <cgrandin@shaw.ca>
2018-08-28 14:02:00 -0700

    Strip out some useless stuff.

--- R/all.R
+++ R/all.R
@@ -5,21 +5,6 @@
 # To debug in an R session, run these 3 commands first:
 # source(file.path(here::here(), "R/all.r"));load.models.into.parent.env();source(file.path(here::here(), "R/custom-knitr-variables.r"))
 
-# rm(list = ls(all = TRUE))
-
-# Need to do this first to provide install.packages.if.needed()
-# if(!("iscamtext" %in% rownames(installed.packages()))){
-#   devtools::install_github("cgrandin/iscamtex")
-# }
-# library(iscamtex)
-
-# install.packages.if.needed("coda")
-# install.packages.if.needed("knitr")
-# install.packages.if.needed("lubridate")
-# install.packages.if.needed("PBSmodelling")
-# install.packages.if.needed("xtable")
-# install.packages.if.needed("RColorBrewer")
-
 library(lubridate)
 library(dplyr)
 library(ggplot2)
@@ -45,11 +30,7 @@ source(file.path(rootd.R, "load-models.R"))
 source(file.path(rootd.R, "mcmc-diagnostics.R"))
 source(file.path(rootd.R, "figures-biomass.R"))
 source(file.path(rootd.data, "get-data.R"))
-##
-## # Code to setup the model names, and start/end years for various things
-## #  in the models
-## source(file.path(rootd.R, "model-setup.R"))
-##
+
 ## # Code to setup forecast model runs
 ## # source("forecast-catch-levels.r")
 ## # Code to setup retro model runs.
