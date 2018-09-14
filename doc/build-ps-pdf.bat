94fc7a65f8a3a148268858324986efa270d1c900
Chris Grandin <cgrandin@shaw.ca>
2018-09-14 10:05:27 -0700

    Delete doc dorectory (old original RNW-based stuff.

--- doc/build-ps-pdf.bat
+++
@@ -1,3 +0,0 @@
-Rscript -e "library(knitr);knit('./res-doc.rnw')" 1> knitr-output.log 2>&1
-
-(@latex -synctex=1 "res-doc.tex" && bibtex "res-doc" && latex "res-doc.tex" && latex "res-doc.tex" && dvips "res-doc.dvi" && ps2pdf "res-doc.ps") 1> latex-output.log 2>&1
